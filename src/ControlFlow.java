30abc33ac6d7cd6876513406202477564a14513d
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2020-04-28 15:59:16 -0500

    added the scanner class

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -1,3 +1,4 @@
+import java.util.Scanner; 
 
 public class ControlFlow {
 
@@ -9,6 +10,29 @@ public class ControlFlow {
 		System.out.println();
 		AsciiChars.printUpperCase("testing");
 		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your name? ");
+		String name = input.nextLine();
+		System.out.println("Hello " + name);
+		
+		boolean again = true;
+		
+		while(again) {
+			System.out.println("Would you like to continue to the interactive portion? ");
+			String answer = input.nextLine();
+			
+			if(answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("y")) {
+				
+				System.out.println(answer);
+				again = false;
+			}
+			else {
+				System.out.println("Please return later to complete the survey");
+				break;
+			}
+			System.out.println("testing");
+		}
 
 	}
 
